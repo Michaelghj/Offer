dcbc2a63d1e722f3e783c193d4cab8f4762af835
Michaelghj <13689107380@163.com>
2019-10-13 23:06:12 +0800

    插入排序

--- src/com/ghj/sort/Insert_sort.java
+++ src/com/ghj/sort/Insert_sort.java
@@ -2,25 +2,19 @@ package com.ghj.sort;
 
 public class Insert_sort {
     public static void insert_sort(int[] arr){
-        if(arr == null || arr.length < 2){
-            return;
-        }
-        for (int i = 0; i < arr.length; i++){
-            int minIndex = i;
-            for (int j = i+1; j > 0; j--){
-                if (arr[j] < arr[minIndex]){
-                    minIndex = j;
-                    swap(arr, i, minIndex);
+        if (arr == null || arr.length < 2){
+            for (int i = 0; i < arr.length - 1; i++){
+                for (int j = i + 1; j > 0; j--){
+                    if (arr[j-1] > arr[j]){
+                        int temp = arr[j];
+                        arr[j] = arr[j-1];
+                        arr[j-1] =  temp;
+                    }else {
+                        //不要交换
+                        break;
+                    }
                 }
             }
-
         }
     }
-
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
 }
